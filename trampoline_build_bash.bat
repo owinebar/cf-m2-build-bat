e9bd7b689f1fcaa46675ee254fc87c33d8057e17
Onnie Lynn Winebarger <owinebar@gmail.com>
2025-01-20 13:41:05 -0500

    Adding trampoline batch scripts for installing into /Scripts.  Untested from that location

---
+++ trampoline_build_bash.bat
@@ -0,0 +1,35 @@
+REM    bld.bat - Build batch file for "conda build" on win-64 trampolining to m2-bash
+REM    Copyright 2025 Onnie Lynn Winebarger
+REM    This program is free software: you can redistribute it and/or modify
+REM    it under the terms of the GNU Affero General Public License as published
+REM    by the Free Software Foundation, either version 3 of the License, or
+REM    (at your option) any later version.
+REM
+REM    This program is distributed in the hope that it will be useful,
+REM    but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM    GNU Affero General Public License for more details.
+REM
+REM    You should have received a copy of the GNU Affero General Public License
+REM    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+REM 
+
+set BUILD_SCRIPT="%1"
+if "%BUILD_SCRIPT%" == "" set "BUILD_SCRIPT=test.sh"
+
+sed -Enf "%0\..\win_build_env.sed" <build_env_setup.bat >build_env_setup.sh
+echo #!/bin/bash >build-win.sh
+echo cd $(cygpath '%CD%') >>build-win.sh
+echo export MSYSTEM=MINGW64 >>build-win.sh
+echo . /etc/msystem >>build-win.sh
+echo . build_env_setup.sh >>build-win.sh
+REM Use this line for debugging the bash shell invocation
+REM echo exec bash -i >>build-win.sh
+echo exec "./%BUILD_SCRIPT" >>build-win.sh
+
+echo set MSYSTEM=MINGW64 >build-win.bat
+echo "%BUILD_PREFIX%\Library\usr\bin\bash.exe" -lec "$(cygpath '%CD%\build-win.sh')"  >>build-win.bat
+
+REM Have to ensure only one cygwin/MSYS DLL is in use at a time
+REM Use this line for debugging the bash shell invocation
+start /B /W /I cmd /c build-win.bat
---
+++ trampoline_test_bash.bat
@@ -0,0 +1,40 @@
+REM    test.bat - Build batch file for "conda build" on win-64 trampolining to m2-bash
+REM    Copyright 2025 Onnie Lynn Winebarger
+REM    This program is free software: you can redistribute it and/or modify
+REM    it under the terms of the GNU Affero General Public License as published
+REM    by the Free Software Foundation, either version 3 of the License, or
+REM    (at your option) any later version.
+REM
+REM    This program is distributed in the hope that it will be useful,
+REM    but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM    GNU Affero General Public License for more details.
+REM
+REM    You should have received a copy of the GNU Affero General Public License
+REM    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+REM 
+
+set TEST_SCRIPT="%1"
+if "%TEST_SCRIPT%" == "" set "TEST_SCRIPT=test.sh"
+
+
+sed -Enf "%0\..\conda_test_env_bat2sh.sed" <conda_test_env_vars.bat >conda_test_env_vars.sh
+echo #!/bin/bash >test-win.sh
+echo cd $(cygpath '%CD%') >>test-win.sh
+echo export MSYSTEM=MINGW64 >>test-win.sh
+echo . /etc/msystem >>test-win.sh
+echo . conda_test_env_vars.sh >>test-win.sh
+REM This is such a hack, but installing binaries in $PREFIX/Library/bin is incorrect
+REM for MSYS-based shells, as MSYS-2.0.dll will treat it as non-existent when referenced as /bin,
+REM and cygpath will translate the explicit full path to /bin
+echo mkdir -p ${PREFIX}/Library/mingw64/bin >>test-win.sh
+echo cp -f "${PREFIX}/Library/bin"/* "${PREFIX}/Library/mingw64/bin/" >>test-win.sh
+REM Use this line for debugging the bash shell invocation
+REM echo exec bash -i >>build-win.sh
+echo exec "./%TEST_SCRIPT" >>build-win.sh
+
+echo set MSYSTEM=MINGW64 >test-win.bat
+echo "%BUILD_PREFIX%\Library\usr\bin\bash.exe" -lec "$(cygpath '%CD%\build-win.sh')"  >>test-win.bat
+
+REM Have to ensure only one cygwin/MSYS DLL is in use at a time
+start /B /W /I cmd /c build-win.bat
