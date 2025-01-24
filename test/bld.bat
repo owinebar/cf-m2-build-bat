158b78fcd95f431a57a4a0cd416cde82809e255c
Onnie Lynn Winebarger <owinebar@gmail.com>
2025-01-24 14:23:39 -0500

    Adapt test case to specify new arguments

--- test/bld.bat
+++ test/bld.bat
@@ -1,3 +1,3 @@
 REM Trampoline to the build shell script
 
-CALL trampoline_build_bash.bat "%RECIPE_DIR%\build.sh"
+CALL trampoline_build_bash.bat MINGW64 "%RECIPE_DIR%\build.sh"
--- test/test.bat
+++ test/test.bat
@@ -1,3 +1,3 @@
 REM Trampoline to the test shell script
 
-CALL trampoline_test_bash.bat test.sh
+CALL trampoline_test_bash.bat MINGW64 test.sh
